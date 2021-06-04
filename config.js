69c68e4c504d86bfa5975fc6140176118bebbff2
KimChon2605 <85360082+KimChon2605@users.noreply.github.com>
2021-06-04 23:17:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,17 +4,17 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'Quang đây',
+    btnIntro: 'Quang nè',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích Quang <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho Quang <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <3',
-    mess: 'Anh biết mà 🥰. Yêu Linh nhiều 🥰🥰',
-    messDesc: 'Tối nay ngủ ngon nha <3.',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay ngủ ngon nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://https://www.facebook.com/linhphuong.hothi.12'
+    messLink: 'http://www.facebook.com/linhphuong.hothi.12'
 }
 
