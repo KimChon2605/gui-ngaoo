845d39f28e5dcd9891f15b69196c9be8b82acfc9
KimChon2605 <85360082+KimChon2605@users.noreply.github.com>
2021-06-04 23:03:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Quang đây',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
@@ -15,5 +15,6 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu Linh nhiều <3',
     messDesc: 'Tối nay ngủ ngon nha <3.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://https://www.facebook.com/linhphuong.hothi.12
+    messLink: 'http://https://www.facebook.com/linhphuong.hothi.12'
 }
+
