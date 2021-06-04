a9aa46dc1eefce2170bc693b424c1d4541999592
KimChon2605 <85360082+KimChon2605@users.noreply.github.com>
2021-06-04 23:44:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,10 +11,10 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <3333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay ngủ ngon nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://https://www.facebook.com/profile.php?id=100035686191678'
+    messLink: 'http://www.facebook.com/profile.php?id=100030279815600'
 }
 
