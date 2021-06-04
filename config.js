c3cb7f833e319e9e42319b60c73751fa4e9155a7
KimChon2605 <85360082+KimChon2605@users.noreply.github.com>
2021-06-04 23:42:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,6 +15,6 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay ngủ ngon nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://https://www.facebook.com/profile.php?id=100035686191678
+    messLink: 'http://https://www.facebook.com/profile.php?id=100035686191678'
 }
 
